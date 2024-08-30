1eede6d1e4420e284a839e4802fa7617df31526a
marlonbarrios <marlon@dance-tech.net>
2024-08-29 23:16:23 -0400

    pangeaspeople

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'bauhaus time traveler by marlon barrios solano ',
-  description: 'bauhaus time traveler | duet in latet space | concept, programming, sound design and performance by marlon barrios solano',
+  title: 'Pangea People ',
+  description: 'Pangea People | duet in latet space | concept, programming, sound design and performance by marlon barrios solano',
 }
 
 export default function RootLayout({
